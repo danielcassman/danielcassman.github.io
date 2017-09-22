dc6ece843bebed914b7d2dfe3cf44cd5a68da199
Daniel Cassman <dcassman@gmail.com>
2017-09-22 11:45:39 -0400

    Lots of fixes and changes

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,4 +1,5 @@
-var STOCKS = ['IWD', 'IWF'];
+/* Global Variables */
+var STOCKS = ['IWD', 'IWF', '^IXIC', '^GSPC', 'ES=F'];
 var DEFAULT_LOCATION = {
 		coords: {
         latitude: 38.8942345,
@@ -10,6 +11,14 @@ var API_KEYS = {
 	openweathermap: 'b2f350cd6c8d249908dd100bba6c6e7c'
 };
 
+/* Function: ready
+ * ---------------
+ * Launches function when the web page has loaded, so
+ * that the function has access to the entire DOM.
+ * See: http://youmightnotneedjquery.com/
+ *
+ * @param fn: The function to executed on page load.
+ */
 function ready(fn) {
 	if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
 		fn();
@@ -19,15 +28,14 @@ function ready(fn) {
 }
 
 function getLocationAndUpdateWeather() {
+	document.getElementById('weather-button').addEventListener('click', function() {
+		fadeToggle(document.getElementById('weather-info'));
+		return false;
+	});
   if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(loadLocalWeather, noLocationAvailable);
   } else {
-    loadLocalWeather({
-      coords: {
-        latitude: 38.8942345,
-        longitude: -77.0295696
-      }
-    });
+    loadLocalWeather(DEFAULT_LOCATION);
   }
 }
 
@@ -39,14 +47,14 @@ function loadLocalWeather(position)	{
 	  if (request.status >= 200 && request.status < 400) {
 		var data = JSON.parse(request.responseText);
 		applyWeather(data);
+		logUpdate("Weather updated from OpenWeatherMap.");
 	  } else {
-		console.log(request.responseText);
-
+		logUpdate("Unable to reach OpenWeatherMap API. " + responseText);
 	  }
 	};
 
 	request.onerror = function() {
-	  // There was a connection error of some sort
+	  logUpdate("Unable to reach OpenWeatherMap API. " + responseText);
 	};
 
 	request.send();
@@ -68,29 +76,28 @@ function loadStocks(symbols) {
 				createStockItem(stocks[i], 'stocks');
 			}
 		}
+		logUpdate("Stocks updated from Yahoo.");
 	  } else {
-		console.log(request.responseText);
-
+		logUpdate("Unable to reach Yahoo Stocks API. " + responseText);
 	  }
 	};
 
 	request.onerror = function() {
-	  // There was a connection error of some sort
+	  logUpdate("There was an error reaching the Yahoo Stocks API.");
 	};
 
 	request.send();
+	
+	
 }
 
 function setUpStocks() {
-	document.getElementById('stocks').style.display = 'none';
+	document.getElementById('stocks').style.opacity = 0;
 	document.getElementById('stocks-button').addEventListener('click', function() {
-		if(document.getElementById('stocks').style.display == 'none') {
-			document.getElementById('stocks').style.display = 'block';
-		} else {
-			document.getElementById('stocks').style.display = 'none';
-		}
+		fadeToggle(document.getElementById('stocks'));
 		return false;
 	});
+	loadStocks(STOCKS);
 }
 
 function createStockItem(stock, id) {
@@ -105,15 +112,16 @@ function createStockItem(stock, id) {
 	// Price
 	var price = document.createElement('span');
 	price.setAttribute('class', 'stock-price');
-	price.textContent = stock.LastTradePriceOnly;
+	price.textContent = decimalPlaces(stock.LastTradePriceOnly, 2);
 	
 	// Change
 	var change = document.createElement('span');
 	var c = stock.Change;
 	if(stock.Change == null)
-		c = Math.floor((stock.LastTradePriceOnly - stock.PreviousClose) * 100) / 100;
+		c = (stock.LastTradePriceOnly - stock.PreviousClose);
+	c = decimalPlaces(c, 2);
 	change.setAttribute('class', 'stock-change ' + (c < 0 ? 'negative' : 'positive'));
-	change.textContent = '(' + (c < 0 ? c : '+' + c) + ')';
+	change.textContent = '(' + (c < 0 ? '' : '+') + c + ')';
 	
 	div.appendChild(sym);
 	div.appendChild(price);
@@ -121,8 +129,32 @@ function createStockItem(stock, id) {
 	document.getElementById(id).appendChild(div);
 }
 
+function decimalPlaces(n, places) {
+	n = +n;
+	
+	n = Math.round(n * Math.pow(10, places)) / Math.pow(10, places);
+	
+	n = 'a' + n;
+	if(n.indexOf('.') == -1) {
+		n = n + '.';
+	}
+	
+	n = n.split('.');
+	
+	var diff = places - n[1].length;
+	
+	for(var i = 0; i < diff; i++) {
+		n[1] += '0';
+	}
+	
+	n[0] = n[0].substring(1);
+	n = n.join('.');
+	
+	return n;
+}
+
 function noLocationAvailable(error) {
-	console.log(error);
+	logUpdate('Location issue: ' + error.message);
 	loadLocalWeather(DEFAULT_LOCATION);
 }
 	
@@ -187,10 +219,125 @@ function vertHoriz() {
 	console.log(ratio);
 }
 
+/* Function: setUpNews
+ * -------------------
+ *
+ */
+function setUpNews() {
+	document.getElementById('news').style.opacity = 0;
+	document.getElementById('news-button').addEventListener('click', function() {
+		fadeToggle(document.getElementById('news'));
+		return false;
+	});
+	updateGuardianHeadlines('us-news', '#us');
+	updateGuardianHeadlines('world', '#world');
+}
+
+function fadeToggle(el) {
+	if(el.style.opacity == 0 && el.style.opacity != "") {
+		fadeIn(el);
+	} else {
+		fadeOut(el);
+	}
+	return false;
+}
+
+function fadeIn(el) {
+  el.style.opacity = 0;
+  el.style.display = 'block';
+
+  var tick = function() {
+    el.style.opacity = +el.style.opacity + 0.02;
+
+    if (+el.style.opacity < 1) {
+      (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 10)
+    }
+  };
+
+  tick();
+}
+
+function fadeOut(el) {
+  el.style.opacity = 1;
+
+  var tick = function() {
+    el.style.opacity = +el.style.opacity - 0.02;
+
+    if (+el.style.opacity > 0) {
+      (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 10)
+    } else {
+		el.style.display = 'none';
+	}
+  };
+
+  tick();
+}
+
+/* Function: updateGuardianHeadlines
+ * ---------------------------------
+ * Updates news from the Guardian.
+ *
+ */
+ function updateGuardianHeadlines(section, wrapper) {
+   var request = new XMLHttpRequest();
+   request.open('GET', "http://content.guardianapis.com/search?section=" + section + "&api-key=5d33b608-c47b-4f64-99c3-59c8deb3c857", true);
+   request.onload = function() {
+     if(request.status >= 200 && request.status < 400) {
+       var data = JSON.parse(request.responseText);
+       var headlines = data.response.results;
+
+       // Remove current articles from the DOM
+       var current_articles = document.querySelector(wrapper).querySelectorAll("a.article");
+       for(var i = 0; i < current_articles.length; i++) {
+         current_articles[i].parentNode.removeChild(current_articles[i]);
+       }
+
+       // Add new articles to the DOM
+       var added = 0;
+       for(var i = 0; (i < headlines.length && added < 5); i++) {
+         var article = headlines[i];
+         if(article.type == 'article') {
+           var link = document.createElement('a');
+           link.setAttribute('class', 'article');
+           link.innerHTML = article.webTitle;
+           link.setAttribute('href', article.webUrl);
+           link.setAttribute('target', '_blank');
+           document.querySelector(wrapper).appendChild(link);
+           added++;
+         }
+       }
+       logUpdate("News updated from The Guardian.");
+     } else {
+       logUpdate("The Guardian API returned an error. News not updated.");
+     }
+   };
+
+   request.onerror = function() {
+     logUpdate("Unable to reach The Guardian website.");
+   }
+
+   request.send();
+   return true;
+ }
+ 
+ /* Function: logUpdate
+ * -------------------
+ * Passes an update message to the log. Currently uses console.log, but could
+ * easily be modified to pass the message anywhere. Adds a timestamp.
+ *
+ * @param msg: The message to send to the log
+ */
+function logUpdate(msg) {
+  d = new Date();
+  console.log(d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate() + " "
+    + d.getHours() + ":" + (d.getMinutes() < 10 ? "0"
+    + d.getMinutes() : d.getMinutes()) + ": " + msg);
+}
+
 function setUp() {
 	getLocationAndUpdateWeather();
 	setUpStocks();
-	loadStocks(STOCKS);
+	setUpNews();
 }
 
 ready(setUp);
